8103724a22da01ebaa7795f06248c3398b3d6e27
Enric <36191828+PerezEnric@users.noreply.github.com>
2020-07-07 01:37:18 +0200

    Idle, walk, run and attack animations added to ninja

---
+++ Project/SummerTime Project/Assets/EnemyNinja/AnimTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("1")) // WALK
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isAttacking", false);
+        }
+
+        if(Input.GetKey("2")) // IDLE
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isAttacking", false);
+        }
+
+        if (Input.GetKey("3")) // RUN
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isAttacking", false);
+        }
+
+        if (Input.GetKey("4")) // ATTACK
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isAttacking", true);
+        }
+    }
+}
