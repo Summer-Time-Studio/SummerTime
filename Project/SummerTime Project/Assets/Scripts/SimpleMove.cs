22cd4b7962d712745be6824e153084b39aabe1c1
VictorTirado <arlaquim@gmail.com>
2020-07-06 17:57:00 +0200

    Ninja added

---
+++ Project/SummerTime Project/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleMove : MonoBehaviour
+{
+    NavMeshAgent myNavMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetDestinationToMousePosition();
+        }
+       
+    }
+    void SetDestinationToMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            myNavMeshAgent.SetDestination(hit.point);
+        }
+    }
+}
