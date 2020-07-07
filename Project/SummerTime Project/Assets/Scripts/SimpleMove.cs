502589a9849d10c0973122e4a522412798fec510
VictorTirado <arlaquim@gmail.com>
2020-07-07 16:11:30 +0200

    Conditions to change animation

--- Project/SummerTime Project/Assets/Scripts/SimpleMove.cs
+++ Project/SummerTime Project/Assets/Scripts/SimpleMove.cs
@@ -5,7 +5,10 @@ using UnityEngine.AI;
 
 public class SimpleMove : MonoBehaviour
 {
-    NavMeshAgent myNavMeshAgent;
+    public Animator anim;
+   
+    public NavMeshAgent myNavMeshAgent;
+    RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,19 +18,36 @@ public class SimpleMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Attack();
         if (Input.GetMouseButtonDown(0))
         {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
             SetDestinationToMousePosition();
+            Debug.Log(this.GetComponent<Transform>().position);
         }
        
     }
     void SetDestinationToMousePosition()
     {
-        RaycastHit hit;
+        
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit))
         {
             myNavMeshAgent.SetDestination(hit.point);
+            Debug.Log(hit.point);
         }
+    }
+
+    void Attack()
+    {
+       float dis = Vector3.Distance(this.GetComponent<Transform>().position, hit.point);
+        Debug.Log(dis);
+       if (dis<0.3)
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", true);
+        }
+        
     }
 }
